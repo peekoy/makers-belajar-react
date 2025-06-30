d520d7255534a5e2a6414e9b12c183f11275dbf0
pekoy <mhmmdfaiz160@gmail.com>
2025-06-30 11:04:40 +0700

    feat: error boundary

---
+++ src/components/Fragments/ErrorBoundary.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className='flex flex-col text-center justify-center items-center h-dvh'>
+          <h2>Oops! Something went wrong.</h2>
+          <p>
+            We are sorry for the inconvenience. Please try refreshing the page.
+          </p>
+          {this.props.showDetails && this.state.error && (
+            <details style={{ whiteSpace: 'pre-wrap', textAlign: 'left' }}>
+              {this.state.error && this.state.error.toString()}
+              <br />
+              {this.state.errorInfo.componentStack}
+            </details>
+          )}
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
