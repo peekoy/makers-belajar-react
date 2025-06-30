bbd3c8dd5adb2b1fe329bcb46ff13313ed586206
pekoy <mhmmdfaiz160@gmail.com>
2025-06-30 11:27:45 +0700

    feat: implement suspense

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { StrictMode } from 'react';
+import { StrictMode, Suspense, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import {
@@ -7,19 +6,20 @@ import {
   RouterProvider,
   Navigate,
 } from 'react-router-dom';
-import LoginPage from './pages/login.jsx';
-import RegisterPage from './pages/register.jsx';
-import ErrorPage from './pages/404.jsx';
-import ProductsPage from './pages/products.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import { CartProvider } from './contexts/cartContext.jsx';
 import ErrorBoundary from './components/Fragments/ErrorBoundary.jsx';
+
+const LoginPage = lazy(() => import('./pages/login.jsx'));
+const RegisterPage = lazy(() => import('./pages/register.jsx'));
+const ProductsPage = lazy(() => import('./pages/products.jsx'));
+const ErrorPage = lazy(() => import('./pages/404.jsx'));
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Navigate to='/login' replace />,
-    // errorElement: <Navigate to='/404' replace />,
+    errorElement: <Navigate to='/404' replace />,
   },
   {
     path: '/login',
@@ -47,7 +47,17 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <CartProvider>
-        <RouterProvider router={router} />
+        <Suspense
+          fallback={
+            <div className='flex justify-center items-center h-dvh '>
+              <p className='text-xl text-center'>
+                Loading page... <br /> Please wait a moment.
+              </p>
+            </div>
+          }
+        >
+          <RouterProvider router={router} />
+        </Suspense>
       </CartProvider>
     </AuthProvider>
   </StrictMode>
