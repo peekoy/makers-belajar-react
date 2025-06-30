46accb2a37cb718b02dc476dad734a4cfa4cbc11
pekoy <mhmmdfaiz160@gmail.com>
2025-06-30 15:30:02 +0700

    feat: link to detail products

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,25 +1,29 @@
 import { memo } from 'react';
 import Button from '../Elements/Button';
+import { Link } from 'react-router-dom';
 
-// Komponen internal bisa tetap di sini atau dipisah jika lebih kompleks
-const Header = ({ image }) => {
+const Header = ({ image, id }) => {
   console.log('render header');
 
   return (
-    <img src={image} alt='product' className='p-8 rounded-t-lg h-60 w-full' />
+    <Link to={`/product/${id}`}>
+      <img src={image} alt='product' className='p-8 rounded-t-lg h-60 w-full' />
+    </Link>
   );
 };
 
-const Body = ({ name, children }) => {
+const Body = ({ name, children, id }) => {
   console.log('render body');
 
   return (
-    <div className='px-5 pb-5 h-full'>
-      <h5 className='text-xl font-semibold tracking-tight text-white'>
-        {name.substring(0, 20)}...
-      </h5>
-      <p className='text-m text-white'>{children.substring(0, 100)}...</p>
-    </div>
+    <Link to={`/product/${id}`}>
+      <div className='px-5 pb-5 h-full'>
+        <h5 className='text-xl font-semibold tracking-tight text-white'>
+          {name.substring(0, 20)}...
+        </h5>
+        <p className='text-m text-white'>{children.substring(0, 100)}...</p>
+      </div>
+    </Link>
   );
 };
 
@@ -42,8 +46,10 @@ const Footer = ({ id, price, handleAddToCart }) => {
 const CardProduct = memo(({ product, handleAddToCart }) => {
   return (
     <div className='w-full max-w-xs bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 my-2 flex flex-col justify-between'>
-      <Header image={product.image} />
-      <Body name={product.title}>{product.description}</Body>
+      <Header image={product.image} id={product.id} />
+      <Body name={product.title} id={product.id}>
+        {product.description}
+      </Body>
       <Footer
         id={product.id}
         price={product.price}
@@ -52,10 +58,5 @@ const CardProduct = memo(({ product, handleAddToCart }) => {
     </div>
   );
 });
-
-// Hapus export terpisah untuk Header, Body, dan Footer jika tidak digunakan di tempat lain
-// CardProduct.Header = Header;
-// CardProduct.Body = Body;
-// CardProduct.Footer = Footer;
 
 export default CardProduct;
